ef17b511ffb92ac6b605e89b6c73dbca5a2a5e6b
Stefan Bogdanovic <bogdanovicstefan1997@gmail.com>
2021-05-09 09:41:24 +0200

    Redis added

---
+++ src/plugins/redis.ts
@@ -0,0 +1,19 @@
+import fp from 'fastify-plugin';
+import { createClient, RedisClient } from 'redis';
+import { configuration } from '../config';
+
+export default fp(async (fastify) =>
+  fastify.decorate(
+    'redis',
+    createClient({
+      host: configuration.redis.host,
+      port: configuration.redis.port
+    })
+  )
+);
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    redis: RedisClient;
+  }
+}
